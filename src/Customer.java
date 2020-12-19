33b3b5256aa152ee04209ff578db1a9a4f56b80c
Kubilay <kubilay.yilmaz@ozu.edu.tr>
2020-12-19 22:17:31 +0300

    IntelliJ Deneme

---
+++ src/Customer.java
@@ -0,0 +1,56 @@
+import java.util.List;
+
+public class Customer {
+
+    private int user_id;
+    private String first_name;
+    private String last_name;
+    private String login_name;
+    private String gender;
+    private String city;
+
+    Product product;
+    //Lists
+    List<Customer> customerList;
+
+    public static void main(String[] args) {
+        Customer customer = new Customer(100, "Kubilay", "Yilmaz", "Login Name", "Male", "Istanbul");
+        showInfos(customer);
+    }
+    Customer(int user_id_, String first_name_, String last_name_, String login_name_, String gender_, String city_){
+        this.user_id = user_id_; // ??? Degismesi gerekecek id'nin, +1 olarak arttir her seferinde
+        this.first_name = first_name_;
+        this.last_name = last_name_;
+        this.login_name = login_name_;
+        this.gender = gender_;
+        this.city = city_;
+    }
+
+    public static void showInfos(Customer customer){
+
+        System.out.println("Customer ID: " + customer.user_id + "\nFirst Name: " + customer.first_name + "\nlast Name: " + customer.last_name + "\nLogin Name: " + customer.login_name + "\nGender: " +  customer.gender + "\nCity: "+  customer.city);
+        // Bu neyin infosu??
+    }
+
+    public void orderProduct(){
+        //product = new Product();
+    }
+
+    // GEREKSIZ?
+    /*public void listPendingOrders(){
+
+    }*/
+
+    public void listPurchasedProducts(){
+
+    }
+
+    public void addCustomer(int user_id_, String first_name_, String last_name_, String login_name_, String gender_, String city_){
+
+        // Eger customerList'te bu customer yoksa ekle.
+        if(!customerList.contains(user_id_)){
+            customerList.add(new Customer(user_id_,first_name_, last_name_, login_name_, gender_, city_));
+        }
+    }
+
+}
--- src/Product.java
+++ src/Product.java
@@ -1,8 +1,8 @@
 import java.util.*;
-import java.util.Locale.Category;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.time.*;
+
 public class Product {
 
     public static void main(String[] args) {
@@ -12,20 +12,21 @@ public class Product {
     }
 
 
+
     ArrayList<Product> productList= new ArrayList<Product>();
     ArrayList<String> categoryList = new ArrayList<String>();
 
-    String productName;
-    String category;
-    String brand;
-    float buyPrice;
-    float sellPrice;
-    int quantity;
-    Date expDate;
-    public static Date now;
-    int productId = 1000;
+    private String productName;
+    private String category;
+    private String brand;
+    private float buyPrice;
+    private float sellPrice;
+    private int quantity;
+    private Date expDate;
+    private static Date now;
+    private int productId = 1000;
 
-    Product(int productId_,String productName_, String category_, String brand_, float buyPrice_, float sellPrice_, int quantity_, Date expDate_){
+    Product(int productId_, String productName_, String category_, String brand_, float buyPrice_, float sellPrice_, int quantity_, Date expDate_){
         this.productId = productId_;
         this.productName = productName_;
         this.category = category_;
@@ -39,33 +40,68 @@ public class Product {
     }
 
     // Method will be called from GUI. Will be connected with database.
-    void AddProduct(int productId_,String productName_, String category_, String brand_, float buyPrice_, float sellPrice_, int quantity_, Date expDate_) {
+    void AddProduct(int productId_ ,String productName_, String category_, String brand_, float buyPrice_, float sellPrice_, int quantity_, Date expDate_) {
         productList.add(new Product(productId_,productName_, category_, brand_, buyPrice_, sellPrice_, quantity_, expDate_));
     }
 
-
     // Method will be called from GUI. Will be connected with database.
     void AddCategory(String category_) {
-
         if(!categoryList.contains(category_)) {
             categoryList.add(category_);
         }
         else {
             System.out.println("This category already exists!");
         }
-
     }
 
 
     // Method will be called from GUI. Will be connected with database.
-    void Sell() {
-        // Ne yapilacak?
+    void Sell(int productId_, int quantity_) {
+        for(Product product : productList) {
+            if(product.productId == productId_ ){
+                if(product.quantity >= quantity_){
+                    if(product.quantity - quantity_ == 0){
+                        System.out.println("Product with ID: " + product.productId + " has successfully been sold");
+                        productList.remove(product);
+                    }
+                    else{
+                        product.quantity -= quantity_;
+                        System.out.println(quantity_ + "of products sold from Product with ID: " + product.productId);
+                    }
+                }
+                else{
+                    System.out.println("There is a quantity mismatch!");
+                }
+            }
+            else{
+                System.out.println("There is no such product with product ID " + productId_);
+            }
+        }
     }
 
 
     // Method will be called from GUI. Will be connected with database.
-    void Remove() {
-        // Ne yapilacak?
+    void Remove(int productId_, int quantity_) {
+        for(Product product : productList) {
+            if(product.productId == productId_ ){
+                if(product.quantity >= quantity_){
+                    if(product.quantity - quantity_ == 0){
+                        System.out.println("Product with ID: " + product.productId + " has successfully been removed");
+                        productList.remove(product);
+                    }
+                    else{
+                        product.quantity -= quantity_;
+                        System.out.println(quantity_ + "of products removed from Product with ID: " + product.productId);
+                    }
+                }
+                else{
+                    System.out.println("There is a quantity mismatch!");
+                }
+            }
+            else{
+                System.out.println("There is no such product with product ID " + productId_);
+            }
+        }
     }
 
 
@@ -75,5 +111,20 @@ public class Product {
         if(this.expDate.compareTo(now) <= 0) {
             System.out.println("Expiration Date Has Expired");
         }
+
+        // veya bu methodu bool yapip -> return (this.expDate.compareTo(now) <= 0);
     }
+
+     void showAll(){
+        for(Product product : productList){
+            System.out.println("Product Name: " + product.productName + "Product ID: " + product.productId + "Category: " + product.category
+                    + "Brand: " + product.brand  + "Buy Price: " +product.buyPrice +"Sell Price: "
+                    + product.sellPrice + "Quantity: " + product.quantity + "Expiration Date: " +product.expDate + "\n");
+        }
+    }
+
+    void filterBy(){
+        // Filter GUI uzerinden yapilacak
+    }
+
 }
