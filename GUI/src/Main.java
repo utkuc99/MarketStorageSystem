57600fe89e189f2733396921fe366f83a0278d91
utkuc99 <utkuc1999@gmail.com>
2020-12-30 15:04:19 +0300

    add product page added

--- GUI/src/Main.java
+++ GUI/src/Main.java
@@ -28,10 +28,77 @@ class gui {
             }
         });
 
+
+        JButton add_product = new JButton("Add Product");
+        add_product.setBounds(150, 20, 120, 25);
+        add_product.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                JFrame g = new JFrame("Add Product");
+
+                JLabel l1,l2,l3,l4,l5,l6,l7;
+                l1=new JLabel("Name");
+                l1.setBounds(30,15, 100,30);
+                l2=new JLabel("Buy Price");
+                l2.setBounds(30,53, 100,30);
+                l3=new JLabel("Sell Price");
+                l3.setBounds(30,91, 100,30);
+                l4=new JLabel("Quantity");
+                l4.setBounds(30,129, 100,30);
+                l5=new JLabel("Category");
+                l5.setBounds(30,167, 100,30);
+                l6=new JLabel("Brand");
+                l6.setBounds(30,205, 100,30);
+                l7=new JLabel("Exp. Date");
+                l7.setBounds(30,243, 100,30);
+
+
+                JTextField F_name = new JTextField();
+                F_name.setBounds(110, 15, 200, 30);
+                JTextField F_buyp=new JTextField();
+                F_buyp.setBounds(110, 53, 200, 30);
+                JTextField F_sellp=new JTextField();
+                F_sellp.setBounds(110, 91, 200, 30);
+                JTextField F_quan = new JTextField();
+                F_quan.setBounds(110, 129, 200, 30);
+                JTextField F_categ=new JTextField();
+                F_categ.setBounds(110, 167, 200, 30);
+                JTextField F_brand=new JTextField();
+                F_brand.setBounds(110, 205, 200, 30);
+                JTextField F_expd=new JTextField();
+                F_expd.setBounds(110, 243, 200, 30);
+
+
+                JButton submit_but=new JButton("Submit");
+                submit_but.setBounds(130,281,80,25);
+
+                g.add(l1);
+                g.add(l2);
+                g.add(l3);
+                g.add(l4);
+                g.add(l5);
+                g.add(l6);
+                g.add(l7);
+                g.add(F_name);
+                g.add(F_buyp);
+                g.add(F_sellp);
+                g.add(F_quan);
+                g.add(F_categ);
+                g.add(F_brand);
+                g.add(F_expd);
+                g.add(submit_but);
+                g.setSize(350,350);
+                g.setLayout(null);
+                g.setVisible(true);
+                g.setLocationRelativeTo(null);
+
+            }
+        });
+
         f.add(view_products);
-        f.setSize(600, 200);//400 width and 500 height
-        f.setLayout(null);//using no layout managers
-        f.setVisible(true);//making the frame visible
+        f.add(add_product);
+        f.setSize(600, 200);
+        f.setLayout(null);
+        f.setVisible(true);
         f.setLocationRelativeTo(null);
     }
 }
