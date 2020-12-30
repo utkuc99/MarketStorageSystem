71653828b8eb60818a04049b610b6515c873b37d
utkuc99 <utkuc1999@gmail.com>
2020-12-30 14:46:05 +0300

    added main menu

---
+++ GUI/src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class gui {
+    public static void main(String args[]) {
+        JFrame f = new JFrame("Main Menu");
+    }
+}
