fa4f959ca9965552fdf2e29f7aafc64398372d40
Monster <yigit.sahin@ozu.edu.tr>
2021-01-03 16:21:07 +0300

    orderProduct method added

--- src/Customer.java
+++ src/Customer.java
@@ -22,14 +22,78 @@ public class Customer extends User {
         try{
             Connection connection =  DriverManager.getConnection(DB_URL,USER,PASS);
             PreparedStatement prep = null;
+            String query = "";
+            ResultSet rs = null;
+
+
+            query = "select * from product where id = ?";
+            prep = connection.prepareStatement(query);
+            prep.setInt(1,productID);
+            rs = prep.executeQuery();
+
+            rs.next();
+            if(count > rs.getInt(8)){
+                JOptionPane.showMessageDialog(null,"There is no such that product");
+                return;
+            }
+
+            int newCount = rs.getInt(8) - count;
+            int seller_id = rs.getInt(2);
+
+
+
+            query = "UPDATE product SET count = ? where( id = ? )";
+            prep = connection.prepareStatement(query);
+            prep.setInt(1,newCount);
+            prep.setInt(2,productID);
+
+            prep.executeUpdate();
+
+            int exist = 0;
+            query = "select * from products_purchased where product_ID = ?";
+            prep = connection.prepareStatement(query);
+            prep.setInt(1,productID);
+            rs = prep.executeQuery();
+
+            int previous = 0;
+            while (rs.next()){
+                exist++;
+                if(exist == 1){
+                    previous = rs.getInt(4);
+                }
+            }
+
+            if(exist == 0){
+                query = "insert into products_purchased values (?, ?, ?, ? )";
+                prep = connection.prepareStatement(query);
+                prep.setInt(1,productID);
+                prep.setInt(2,seller_id);
+                prep.setInt(3,customerID);
+                prep.setInt(4,count);
+
+                prep.executeUpdate();
+
+            }else{
+                query = "UPDATE products_purchased SET count = ? where( product_ID = ? )";
+                prep = connection.prepareStatement(query);
+                prep.setInt(1,(count+previous));
+                prep.setInt(2,productID);
+                prep.executeUpdate();
+            }
 
             if(connection != null){ connection.close();  }
 
         }catch (Exception exc){
             exc.printStackTrace();
         }
-
     }
+
+
+
+
+
+
+
 
 
 }
