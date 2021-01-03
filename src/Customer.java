daee7cdaef6adaac931962cb3483dbfca2485b28
Monster <yigit.sahin@ozu.edu.tr>
2021-01-03 16:30:34 +0300

    ArrayList method added

--- src/Customer.java
+++ src/Customer.java
@@ -88,6 +88,43 @@ public class Customer extends User {
         }
     }
 
+    public static ArrayList<Product> listPurchasedProducts(int customerID){
+        ArrayList<Product> purchased_product = new ArrayList<Product>();
+
+        try{
+            Connection connection =  DriverManager.getConnection(DB_URL,USER,PASS);
+            PreparedStatement prep = null;
+            String query = "";
+            ResultSet productsSet = null;
+
+            query = "select * from products_purchased , product where customer_ID = ?";
+            prep = connection.prepareStatement(query);
+            prep.setInt(1,customerID);
+            productsSet = prep.executeQuery();
+            while (productsSet.next()){
+
+                int id = productsSet.getInt(1);
+                int seller_id = productsSet.getInt(2);
+                int count = productsSet.getInt(4);
+                String name = productsSet.getString(7);
+                double price = productsSet.getDouble(8);
+                String category = productsSet.getString(9);
+                String colour = productsSet.getString(10);
+                String description = productsSet.getString(11);
+
+                Product p = new Product(id,seller_id,name,price,category,colour,description,count);
+                purchased_product.add(p);
+
+            }
+
+            if(connection!= null){ connection.close();  }
+
+        }catch (Exception exc){
+            exc.printStackTrace();
+        }
+        return purchased_product;
+    }
+
 
 
 
