76a1c6b0616ad646528c85c2cf8e368cfcdb4fa0
Monster <yigit.sahin@ozu.edu.tr>
2021-01-03 16:04:07 +0300

    database connected

--- src/Customer.java
+++ src/Customer.java
@@ -1,57 +1,35 @@
-import java.util.List;
+import javax.swing.*;
+import java.sql.*;
+import java.util.ArrayList;
 
-public class Customer {
+import static javax.swing.JOptionPane.showMessageDialog;
 
-    private int user_id = 1000;
-    private String first_name;
-    private String last_name;
-    private String login_name;
-    private String gender;
-    private String city;
 
-    List<Customer> customerList;
+public class Customer extends User {
 
-    public static void main(String[] args) {
-        Customer customer = new Customer(100, "Kubilay", "Yilmaz", "Login Name", "Male", "Istanbul");
-        showInfos(customer);
-    }
+    static final String DB_URL = "jdbc:mysql://localhost:3306/marketstoragesystem";
+    static final String USER = "root";
+    static final String PASS = "Ys6912";
 
-    Customer(int user_id_, String first_name_, String last_name_, String login_name_, String gender_, String city_){
-        this.user_id = user_id_;
-        this.first_name = first_name_;
-        this.last_name = last_name_;
-        this.login_name = login_name_;
-        this.gender = gender_;
-        this.city = city_;
-        this.user_id+=1;
-    }
 
-    public static void showInfos(Customer customer){
-        System.out.println("Customer ID: " + customer.user_id + "\nFirst Name: " + customer.first_name +
-                "\nlast Name: " + customer.last_name + "\nLogin Name: " + customer.login_name +
-                "\nGender: " +  customer.gender + "\nCity: "+  customer.city);
-    }
-
-    // Orders Class'i ile baglanacak
-    public void orderProduct(){
-        //product = new Product();
-    }
-
-    // Orders Class'i ile baglanacak
-    public void listPendingOrders(){
+    public Customer(){ super();}
+    public Customer(int id_, String firstName_, String lastName_, String loginName_, String gender_, String city_) {
+        super(id_, firstName_, lastName_, loginName_, gender_, city_);
     }
 
 
-    public void listPurchasedProducts(){
+    public static void orderProduct(int productID, int customerID, int count){
+        try{
+            Connection connection =  DriverManager.getConnection(DB_URL,USER,PASS);
+            PreparedStatement prep = null;
 
-    }
+            if(connection != null){ connection.close();  }
 
-    public void addCustomer(int user_id_, String first_name_, String last_name_, String login_name_, String gender_, String city_){
-
-        // Eger customerList'te bu customer yoksa ekle.
-        if(!customerList.contains(user_id_)){
-            customerList.add(new Customer(user_id_,first_name_, last_name_, login_name_, gender_, city_));
+        }catch (Exception exc){
+            exc.printStackTrace();
         }
+
     }
+
 
 }
