e01155081ce8f703d3966a5f8e55b75467837f93
Monster <yigit.sahin@ozu.edu.tr>
2021-01-03 16:47:31 +0300

    Product class added

--- src/Product.java
+++ src/Product.java
@@ -1,135 +1,38 @@
-import java.util.*;
-import java.text.SimpleDateFormat;
-
 public class Product {
 
-    public static void main(String[] args) {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        now = new Date();
-        System.out.println(formatter.format(now));
+    public int id_;
+    public int seller_id_;
+    public String name;
+    public double price_;
+    public String category_;
+    public String colour_;
+    public String description_;
+    public int count_ = 0;
+
+
+    public Product(){ }
+
+    public Product(int id_, int seller_id_, String name,
+                   double price_, String category_, String colour_, String description_, int count_) {
+        this.id_ = id_;
+        this.seller_id_ = seller_id_;
+        this.name = name;
+        this.price_ = price_;
+        this.category_ = category_;
+        this.colour_ = colour_;
+        this.description_ = description_;
+        this.count_ = count_;
     }
 
-
-
-    ArrayList<Product> productList= new ArrayList<Product>();
-    ArrayList<String> categoryList = new ArrayList<String>();
-
-    private int productId = 1000;
-    private String productName;
-    private String category;
-    private float product_price;
-    private String product_color;
-    private String product_description;
-    private int seller_id;// Seller Class'i olusturulunca eklenecek
-    private int quantity;
-    private Date expDate;
-
-    private String brand;
-    private float sellPrice;
-    private static Date now;
-
-
-    Product(int productId_, String productName_, Category category_, float product_price, float sellPrice_, String product_color_, int quantity_, Date expDate_, String brand_){
-        this.productId = productId_;
-        this.productName = productName_;
-        this.category = category_.product_category_code;
-        this.product_price = product_price;
-        this.sellPrice = sellPrice_;
-        this.product_color = product_color_;
-        this.product_description = category_.product_category_description;
-        this.quantity = quantity_;
-        this.expDate = expDate_;
-        this.brand = brand_;
-        AddCategory(category_);
-        productId+=1;
-    }
-
-    // Method will be called from GUI. Will be connected with database.
-    void AddProduct(int productId_, String productName_, Category category_, float product_price, float sellPrice_, String product_color_, int quantity_, Date expDate_, String brand_) {
-        productList.add(new Product(productId_,productName_, category_, product_price, sellPrice_, product_color_, quantity_, expDate_, brand_));
-    }
-
-    // Method will be called from GUI. Will be connected with database.
-    void AddCategory(Category category_) {
-        if(!categoryList.contains(category_.product_category_code)) {
-            categoryList.add(category_.product_category_code);
-        }
-        else {
-            System.out.println("This category already exists!");
-        }
-    }
-
-
-    // Method will be called from GUI. Will be connected with database.
-    void Sell(int productId_, int quantity_) {
-        for(Product product : productList) {
-            if(product.productId == productId_ ){
-                if(product.quantity >= quantity_){
-                    if(product.quantity - quantity_ == 0){
-                        System.out.println("Product with ID: " + product.productId + " has successfully been sold");
-                        productList.remove(product);
-                    }
-                    else{
-                        product.quantity -= quantity_;
-                        System.out.println(quantity_ + "of products sold from Product with ID: " + product.productId);
-                    }
-                }
-                else{
-                    System.out.println("There is a quantity mismatch!");
-                }
-            }
-            else{
-                System.out.println("There is no such product with product ID " + productId_);
-            }
-        }
-    }
-
-
-    // Method will be called from GUI. Will be connected with database.
-    void Remove(int productId_, int quantity_) {
-        for(Product product : productList) {
-            if(product.productId == productId_ ){
-                if(product.quantity >= quantity_){
-                    if(product.quantity - quantity_ == 0){
-                        System.out.println("Product with ID: " + product.productId + " has successfully been removed");
-                        productList.remove(product);
-                    }
-                    else{
-                        product.quantity -= quantity_;
-                        System.out.println(quantity_ + "of products removed from Product with ID: " + product.productId);
-                    }
-                }
-                else{
-                    System.out.println("There is a quantity mismatch!");
-                }
-            }
-            else{
-                System.out.println("There is no such product with product ID " + productId_);
-            }
-        }
-    }
-
-
-    // Method will be called from GUI. Will be connected with database.
-    void CheckExpDate() {
-        // Eger product'in tarihi bugunse veya gecmisse tarihi gecti olarak isaretle
-        if(this.expDate.compareTo(now) <= 0) {
-            System.out.println("Expiration Date Has Expired");
-        }
-
-        // veya bu methodu bool yapip -> return (this.expDate.compareTo(now) <= 0);
-    }
-
-     void showAll(){
-        for(Product product : productList){
-            System.out.println("Product Name: " + product.productName + " Product ID: " + product.productId + " Brand: " + product.brand +
-                    " Category: " + product.category + " Price: " + product.product_price + " Sell Price: " + product.sellPrice + " Quantity: " + product.quantity +
-                    " Expiration Date: " +product.expDate + "\n");
-        }
-    }
-
-    void filterBy(){
-        // Filter GUI uzerinden yapilacak
+    public Product(int id_, int seller_id_, String name,
+                   double price_, String category_, String colour_, String description_) {
+        this.id_ = id_;
+        this.seller_id_ = seller_id_;
+        this.name = name;
+        this.price_ = price_;
+        this.category_ = category_;
+        this.colour_ = colour_;
+        this.description_ = description_;
     }
 
 }
