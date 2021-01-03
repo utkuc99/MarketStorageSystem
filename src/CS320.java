a810e64de5af31f4337ff1c87edd2eaf22913e4c
eray.erdogan <eray.erdogan@ozu.edu.tr>
2021-01-03 16:27:37 +0300

    Addproduct method added in the seller and defined in main

--- src/CS320.java
+++ src/CS320.java
@@ -64,6 +64,10 @@ public class CS320 {
 
 
 
+    Seller product1= new Seller();
+   // product1.addProduct("GIDA","EKMEK",1.50,"Blue","That is for white's",10,1);
+
+
 
 
 
--- src/Seller.java
+++ src/Seller.java
@@ -7,6 +7,52 @@ public class Seller extends User {
         super();
     }
 
+    void addProduct (String product_category_code, String product_name, double price, String product_color, String product_description,int count, int seller_id )
+    {
+        try{
+            Connection myCon =  DriverManager.getConnection(url,USER,PASS);
+            PreparedStatement myPrepSt = null;
+            String query = "";
+            ResultSet rs = null;
+
+            // Customer burada ana ekrandaki Add User butonuna basacak ve Customer bilgilerini girecek
+            // id otomatik olarak atanıyor
+            int idp = 0;
+            query = "select max(product_id) from product";
+            myPrepSt = myCon.prepareStatement(query);
+            rs = myPrepSt.executeQuery();
+            while (rs.next()){
+                idp = rs.getInt(1);
+            }
+            idp ++ ;
+
+
+// Database Statement
+            query = "insert into product values (?, ?, ?, ?, ?, ?, ?, ?)";
+
+            myPrepSt = myCon.prepareStatement(query);
+            myPrepSt.setInt(1,idp);
+            myPrepSt.setString(2,product_category_code);
+            myPrepSt.setString(3,product_name);
+            myPrepSt.setDouble(4,price);
+            myPrepSt.setString(5,product_color);
+            myPrepSt.setString(6,product_description);
+            myPrepSt.setInt(7,count);
+            myPrepSt.setInt(8,seller_id);
+
+            myPrepSt.executeUpdate();
+
+            if(myCon != null){ myCon.close();  }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    };
+
+
+
+
 
 
 }
